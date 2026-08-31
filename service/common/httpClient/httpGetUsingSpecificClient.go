ca7576e94a7bda370181efceddee41d84f1e7524
Kirill <k.shamilin@gmail.com>
2026-08-31 08:16:12 +0000

    fixed HttpGetSubscriptionWithClient

--- service/common/httpClient/httpGetUsingSpecificClient.go
+++ service/common/httpClient/httpGetUsingSpecificClient.go
@@ -50,31 +50,31 @@ func HttpGetSubscriptionWithClient(c *http.Client, url string) (resp *http.Respo
 	if resp == nil {
 		return nil, fmt.Errorf("httpGetUsingSpecificClient: response is nil")
 	}
-	return
+	return resp, err
 }
 
 // HttpGetSubscriptionWithClient performs HTTP GET request for subscription with HWID headers
-func HttpGetSubscriptionWithClient(c *http.Client, url string) (resp *http.Response, err error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return
-	}
-	
-	// Set User-Agent
-	req.Header.Set("User-Agent", fmt.Sprintf("v2rayA/%v WebRequestHelper", conf.Version))
-	
-	// Get HWID and system info
-	hwid := conf.GetHWID()
-	sysInfo := common.GetSystemInfo()
-	
-	// Set HWID headers
-	req.Header.Set("x-hwid", hwid)
-	req.Header.Set("x-device-os", sysInfo.DeviceOS)
-	req.Header.Set("x-ver-os", sysInfo.VersionOS)
-	req.Header.Set("x-device-model", sysInfo.DeviceModel)
-	
-	if resp, err = c.Do(req); err != nil {
-		resp, err = http.DefaultClient.Do(req)
-	}
-	return
-}
+//func HttpGetSubscriptionWithClient(c *http.Client, url string) (resp *http.Response, err error) {
+//	req, err := http.NewRequest("GET", url, nil)
+//	if err != nil {
+//		return
+//	}
+//	
+//	// Set User-Agent
+//	req.Header.Set("User-Agent", fmt.Sprintf("v2rayA/%v WebRequestHelper", conf.Version))
+//	
+//	// Get HWID and system info
+//	hwid := conf.GetHWID()
+//	sysInfo := common.GetSystemInfo()
+//	
+//	// Set HWID headers
+//	req.Header.Set("x-hwid", hwid)
+//	req.Header.Set("x-device-os", sysInfo.DeviceOS)
+//	req.Header.Set("x-ver-os", sysInfo.VersionOS)
+//	req.Header.Set("x-device-model", sysInfo.DeviceModel)
+//	
+//	if resp, err = c.Do(req); err != nil {
+//		resp, err = http.DefaultClient.Do(req)
+//	}
+//	return
+//}
